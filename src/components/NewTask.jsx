cc27001046daed55f892a512dc97b08ff3fb0678
Dariusz Okonski <darek200180@gmail.com>
2020-04-24 22:05:11 +0100

    add NewTask component

---
+++ src/components/NewTask.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NewTask = () => {
+    return ( 
+        <section className="new-task">
+            <p className="new-task__paragraph">Add new task to list</p>
+            <form>
+                <input type="text" placeholder="Add New Task..."/>
+                <button>
+                    <i class="fas fa-plus"></i>
+                </button>
+            </form>
+        </section>
+     );
+}
+ 
+export default NewTask;
