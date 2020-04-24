1cb99a4d0fac48716bfebf8a41458f0549b78b6f
Dariusz Okonski <darek200180@gmail.com>
2020-04-24 23:04:11 +0100

    provide function removeTask and add to onClick at button delete

--- src/components/SingleTask.jsx
+++ src/components/SingleTask.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { ToDoContext } from '../contexts/ToDoContext';
 
 const SingleTask = ({id, task}) => {
-    const {toggleIsEdit} = useContext(ToDoContext)
+    const {toggleIsEdit, removeTask} = useContext(ToDoContext)
 
     return (  
         <li className="single-task">
@@ -12,7 +12,7 @@ const SingleTask = ({id, task}) => {
                 <button onClick={() => toggleIsEdit(id)}>
                     <i className="fas fa-pen-fancy"></i>
                 </button>
-                <button>
+                <button onClick={() => removeTask(id)}>
                     <i className="fas fa-trash-alt"></i>
                 </button>
             </div>
