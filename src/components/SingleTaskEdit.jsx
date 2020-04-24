f7796d4fe99baaafc7053adf7cee6b5fd76fc195
Dariusz Okonski <darek200180@gmail.com>
2020-04-24 22:54:06 +0100

    create cuurentTask state end send it to Context by editTask function

--- src/components/SingleTaskEdit.jsx
+++ src/components/SingleTaskEdit.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { ToDoContext } from '../contexts/ToDoContext';
 
-const SingleTaskEdit = () => {
+const SingleTaskEdit = ({id, task}) => {
+    const {editTask} = useContext(ToDoContext);
+    const [currentTask, setCurrentTask] = useState(task);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        editTask(id, currentTask);
+    }
     return ( 
         <li className="single-task">
-            <form className="single-task__form">
+            <form onSubmit={handleSubmit} className="single-task__form">
                 <input 
                     type="text" 
-                    value="walk a dog" 
-                    onChange={() => console.log('onChange')}
+                    value={currentTask} 
+                    onChange={(e) => setCurrentTask(e.target.value)}
                 />
                 <button>
                     <i className="fas fa-save"></i>
