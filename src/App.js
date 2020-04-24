333f94c0d1032f8e4544a376593e1aac4686fdd2
Dariusz Okonski <darek200180@gmail.com>
2020-04-24 21:21:20 +0100

    display temporary SingleTask component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import SingleTask from './components/SingleTask';
 
 function App() {
   return (
     <div className="app">
       <Header/>
+
+      <ul>
+        <SingleTask/>
+        <SingleTask/>
+        <SingleTask/>
+      </ul>
     </div>
   );
 }
