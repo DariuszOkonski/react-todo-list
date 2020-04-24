e2045a4080aa7c65ff9518b7c3ebe81564fc0fee
Dariusz Okonski <darek200180@gmail.com>
2020-04-24 22:33:09 +0100

    wrap components Tasks and NewTask with ToDoContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Header from './components/Header.jsx';
 import NewTask from './components/NewTask.jsx';
 import Tasks from './components/Tasks.jsx';
+import ToDoContextProvider from './contexts/ToDoContext.jsx';
 
 function App() {
   return (
     <div className="app">
       <Header/>
 
-      <Tasks/>
-
-      <NewTask/>
+      <ToDoContextProvider>
+        <Tasks/>
+        <NewTask/>
+      </ToDoContextProvider>
     </div>
   );
 }
