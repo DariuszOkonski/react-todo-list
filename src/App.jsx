6c75bf335290a4a11047b81bd80974e0653f31a8
Dariusz Okonski <darek200180@gmail.com>
2020-04-24 22:14:00 +0100

    display Tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import Header from './components/Header.jsx';
-import SingleTask from './components/SingleTask.jsx';
-import SingleTaskEdit from './components/SingleTaskEdit.jsx';
 import NewTask from './components/NewTask.jsx';
+import Tasks from './components/Tasks.jsx';
 
 function App() {
   return (
     <div className="app">
       <Header/>
 
-      <ul>
-        <SingleTask/>  
-        <SingleTask/>  
-        <SingleTaskEdit/>  
-      </ul>  
+      <Tasks/>
 
       <NewTask/>
     </div>
