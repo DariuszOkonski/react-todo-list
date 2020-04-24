4b0b7719b9dd03dced56dc2c9cf0c8b2ebbbc5b3
Dariusz Okonski <darek200180@gmail.com>
2020-04-24 23:12:30 +0100

    add currentTask state and send it by addTask method

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { ToDoContext } from '../contexts/ToDoContext';
 
 const NewTask = () => {
+    const {addTask} = useContext(ToDoContext);
+    const [currentTask, setCurrentTask] = useState(''); 
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addTask(currentTask);
+        setCurrentTask('');
+    }
     return ( 
         <section className="new-task">
             <p className="new-task__paragraph">Add new task to list</p>
-            <form>
-                <input type="text" placeholder="Add New Task..."/>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="Add New Task..."
+                    value={currentTask}
+                    onChange={(e) => setCurrentTask(e.target.value)}
+                    required
+                />
                 <button>
                     <i className="fas fa-plus"></i>
                 </button>
