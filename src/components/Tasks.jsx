af45b15668160b4bade0ec4c97d76b2decb102dc
Dariusz Okonski <darek200180@gmail.com>
2020-04-24 22:32:23 +0100

    provide component with ToDoContext tasks and map after it

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import SingleTask from './SingleTask';
 import SingleTaskEdit from './SingleTaskEdit';
+import { ToDoContext } from '../contexts/ToDoContext';
 
 const Tasks = () => {
+    const {tasks} = useContext(ToDoContext);
+
     return ( 
         <ul>
-            <SingleTask/>
-            <SingleTask/>
-            <SingleTask/>
-            <SingleTask/>
-            <SingleTaskEdit/>
+            {
+              tasks.map(task => {
+                  if(!task.isEdit) {
+                      return <SingleTask key={task.id} {...task} />
+                } else {
+                    return <SingleTaskEdit key={task.id} {...task}/>
+                  }
+              })  
+            }
         </ul>
      );
 }
