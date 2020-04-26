3296628c09d1a184cb1947186b1ce99d5160a67f
Dariusz Okonski <darek200180@gmail.com>
2020-04-26 21:16:31 +0100

    remove hardcoded list

--- src/contexts/ToDoContext.jsx
+++ src/contexts/ToDoContext.jsx
@@ -4,11 +4,7 @@ import {v4 as uuidv4} from 'uuid';
 export const ToDoContext = createContext();
 
 const ToDoContextProvider = (props) => {
-    const [tasks, setTasks] = useState([
-        {id: 1, task: "Go for a walk", isEdit: false},
-        {id: 2, task: "Buy some milk", isEdit: false},
-        {id: 3, task: "Go for schopping", isEdit: false}
-    ]);
+    const [tasks, setTasks] = useState([]);
 
     const addTask = (newTask) => {
         setTasks([...tasks, {id: uuidv4(), task: newTask, isEdit: false}]);
